d16a795f322e345614081b2d73c7774867c85958
RaulCoretB <raulcoret8@gmail.com>
2024-11-25 22:52:51 +0100

    marc ya funciona bien

--- RabURP_Project/Assets/RaBURP_Root/Scripts/Puntuación.cs
+++ RabURP_Project/Assets/RaBURP_Root/Scripts/Puntuación.cs
@@ -5,28 +5,13 @@ using UnityEngine;
 public class Puntuación : MonoBehaviour
 {
 
-    public Text scoreText; // Referencia al componente Text donde se mostrarán los puntos
-    private int score = 0; // Puntuación inicial
-
-    // Método para incrementar los puntos
-    public void AddScore(int points)
-    {
-        score += points;
-        UpdateScoreText();
-    }
-
-    // Método para actualizar el texto en pantalla con la puntuación actual
-    private void UpdateScoreText()
-    {
-        scoreText.text = "Puntos: " + score;
-    }
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // Asegurarnos de que el texto se actualice desde el inicio
-        UpdateScoreText();
+       
     }
 
     // Update is called once per frame
